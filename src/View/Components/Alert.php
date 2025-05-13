eb28d580803b2b4daf4462c7eec3489ceb1a6cec
Kael <kael.cadranel00@gmail.com>
2025-05-13 16:11:53 +0200

    Create alert

---
+++ src/View/Components/Alert.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace LivewireUIKit\View\Components;
+
+use Illuminate\View\Component;
+use Illuminate\View\View;
+
+class Alert extends Component
+{
+    public function __construct(
+        public string $variant = '',
+        public string $message = '',
+        public bool $dismissible = false,
+        public string $title = '',
+        public string $icon = '',
+    )
+    {
+    }
+
+    public function variantClass(): string
+    {
+        return match ($this->variant) {
+            'primary' => 'bg-primary text-primary-content',
+            'secondary' => 'bg-gray-500 text-white',
+            'success' => 'bg-green-500 text-white',
+            'danger' => 'bg-red-500 text-white',
+            'warning' => 'bg-yellow-500 text-white',
+            'info' => 'bg-blue-500 text-white',
+            'light' => 'bg-gray-200 text-gray-800',
+            'dark' => 'bg-gray-800 text-white',
+            default => 'bg-blue-500 text-white',
+        };
+    }
+
+    public function render(): View
+    {
+        return view('livewire-ui-kit::components.alert');
+    }
+}
