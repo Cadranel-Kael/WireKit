9706627ad2ef970623715f4de5b8ba90e6dd45cd
Kael <kael.cadranel00@gmail.com>
2025-05-13 16:12:53 +0200

    Create icon

---
+++ resources/views/components/icon.blade.php
@@ -0,0 +1,1 @@
+<x-dynamic-component :component="$iconSet . '-'. $name" {{ $attributes->class('w-6 h-6') }} />
---
+++ src/View/Components/Icon.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace LivewireUIKit\View\Components;
+
+use Illuminate\View\Component;
+use Illuminate\View\View;
+
+class Icon extends Component
+{
+    public function __construct(
+        public string $name = '',
+        public string $iconSet = 'heroicon'
+    )
+    {
+    }
+
+    public function render(): View
+    {
+        return view('livewire-ui-kit::components.icon');
+    }
+}
