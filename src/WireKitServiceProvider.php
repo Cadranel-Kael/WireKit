fc0966665a512cfb14adf5997485892daf8d17c5
Kael <kael.cadranel00@gmail.com>
2026-02-02 20:17:12 +0100

    Refactor Blade component registration and add tag compiler
    Moved Blade component registration to a dedicated method and introduced a custom WireKitTagCompiler for handling <wire:*> tags. This change streamlines component registration using namespaces and enables precompilation of custom wire components in Blade templates.

--- src/WireKitServiceProvider.php
+++ src/WireKitServiceProvider.php
@@ -17,67 +17,36 @@ class WireKitServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        include_once(__DIR__.'/helpers.php');
+        include_once __DIR__.'/helpers.php';
 
+        $this->registerBladeComponent();
+        $this->compileBladeComponent();
+    }
+
+    /**
+     * Register the Views and sets the component namespace.
+     */
+    protected function registerBladeComponent(): void
+    {
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'wire-kit');
+        Blade::componentNamespace('WireKit\\View\\Components', 'wire');
+    }
 
-        Blade::component('sidebar', View\Components\Sidebar::class);
-        Blade::component('menu-item', View\Components\MenuItem::class);
-//        Blade::component('sub-menu', View\Components\SubMenu::class);
-        Blade::component('search', View\Components\Search::class);
-//        Blade::component('tabs', View\Components\Tabs::class);
-//        Blade::component('tab', View\Components\Tab::class);
-        Blade::component('icon', View\Components\Icon::class);
-        Blade::component('input', View\Components\Input::class);
-        Blade::component('layouts.auth.simple', View\Components\Layouts\Auth\Simple::class);
-        Blade::component('checkbox', View\Components\Checkbox\Index::class);
-        Blade::component('checkbox.group', View\Components\Checkbox\Group::class);
-        Blade::component('select', View\Components\Select\Index::class);
-        Blade::component('select.option', View\Components\Select\Option::class);
-        Blade::component('radio', View\Components\Radio\Index::class);
-        Blade::component('radio.group', View\Components\Radio\Group::class);
-        Blade::component('label', View\Components\Label::class);
-        Blade::component('field', View\Components\Field::class);
-        Blade::component('fieldset', View\Components\Fieldset::class);
-        Blade::component('legend', View\Components\Legend::class);
+    /**
+     * Precompiles the blade components to be prepared for compilation from blade.
+     */
+    protected function compileBladeComponent(): void
+    {
+        $compiler = new WireKitTagCompiler(
+            app('blade.compiler')->getClassComponentAliases(),
+            app('blade.compiler')->getClassComponentNamespaces(),
+            app('blade.compiler')
+        );
 
-        Blade::component('accordion', View\Components\Accordion\Index::class);
-        Blade::component('accordion.item', View\Components\Accordion\Item::class);
-        Blade::component('accordion.heading', View\Components\Accordion\Heading::class);
-        Blade::component('accordion.content', View\Components\Accordion\Content::class);
+        app()->bind('wirekit.compiler', fn () => $compiler);
 
-        Blade::component('alert', View\Components\Alert\Index::class);
-        Blade::component('alert.heading', View\Components\Alert\Heading::class);
-        Blade::component('alert.description', View\Components\Alert\Description::class);
-
-        Blade::component('avatar', View\Components\Avatar\Index::class);
-
-        Blade::component('badge', View\Components\Badge\Index::class);
-        Blade::component('badge.close', View\Components\Badge\Close::class);
-
-        Blade::component('button', View\Components\Button\Index::class);
-        Blade::component('button.group', View\Components\Button\Group::class);
-
-        Blade::component('breadcrumbs', View\Components\Breadcrumbs\Index::class);
-        Blade::component('breadcrumbs.item', View\Components\Breadcrumbs\Item::class);
-
-        Blade::component('context', View\Components\Context::class);
-
-        Blade::component('dropdown', View\Components\Dropdown\Index::class);
-        Blade::component('dropdown.button', View\Components\Dropdown\Button::class);
-        Blade::component('dropdown.menu', View\Components\Dropdown\Menu::class);
-        Blade::component('dropdown.item', View\Components\Dropdown\Item::class);
-
-        Blade::component('menu', View\Components\Menu\Index::class);
-        Blade::component('menu.item', View\Components\Menu\Item::class);
-
-        Blade::component('table', View\Components\Table\Index::class);
-        Blade::component('table.columns', View\Components\Table\Columns::class);
-        Blade::component('table.column', View\Components\Table\Column::class);
-        Blade::component('table.rows', View\Components\Table\Rows::class);
-        Blade::component('table.row', View\Components\Table\Row::class);
-        Blade::component('table.cell', View\Components\Table\Cell::class);
-
-        Blade::component('tooltip', View\Components\Tooltip\Index::class);
+        app('blade.compiler')->precompiler(function ($in) use ($compiler) {
+            return $compiler->compile($in);
+        });
     }
 }
---
+++ src/WireKitTagCompiler.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace WireKit;
+
+use Illuminate\View\Compilers\ComponentTagCompiler;
+
+class WireKitTagCompiler extends ComponentTagCompiler
+{
+    /**
+     * Compile the opening tags within the given string.
+     *
+     * @return string
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function compileOpeningTags(string $value)
+    {
+        $pattern = "/
+            <
+                \s*
+                wire[\:]([\w\-\:\.]*)
+                (?<attributes>
+                    (?:
+                        \s+
+                        (?:
+                            (?:
+                                @(?:class)(\( (?: (?>[^()]+) | (?-1) )* \))
+                            )
+                            |
+                            (?:
+                                @(?:style)(\( (?: (?>[^()]+) | (?-1) )* \))
+                            )
+                            |
+                            (?:
+                                \{\{\s*\\\$attributes(?:[^}]+?)?\s*\}\}
+                            )
+                            |
+                            (?:
+                                (\:\\\$)(\w+)
+                            )
+                            |
+                            (?:
+                                [\w\-:.@%]+
+                                (
+                                    =
+                                    (?:
+                                        \\\"[^\\\"]*\\\"
+                                        |
+                                        \'[^\']*\'
+                                        |
+                                        [^\'\\\"=<>]+
+                                    )
+                                )?
+                            )
+                        )
+                    )*
+                    \s*
+                )
+                (?<![\/=\-])
+            >
+        /x";
+
+        return preg_replace_callback($pattern, function (array $matches) {
+            $this->boundAttributes = [];
+
+            $attributes = $this->getAttributesFromAttributeString($matches['attributes']);
+
+            // Map "button" → "button.index"
+            $component = $matches[1];
+            if (! str_contains($component, '.')) {
+                $component .= '.index';
+            }
+
+            return $this->componentString("wire::$component", $attributes);
+        }, $value);
+    }
+
+    /**
+     * Compile the self-closing tags within the given string.
+     *
+     * @return string
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function compileSelfClosingTags(string $value)
+    {
+        $pattern = "/
+            <
+                \s*
+                wire[\:]([\w\-\:\.]*)
+                \s*
+                (?<attributes>
+                    (?:
+                        \s+
+                        (?:
+                            (?:
+                                @(?:class)(\( (?: (?>[^()]+) | (?-1) )* \))
+                            )
+                            |
+                            (?:
+                                @(?:style)(\( (?: (?>[^()]+) | (?-1) )* \))
+                            )
+                            |
+                            (?:
+                                \{\{\s*\\\$attributes(?:[^}]+?)?\s*\}\}
+                            )
+                            |
+                            (?:
+                                (\:\\\$)(\w+)
+                            )
+                            |
+                            (?:
+                                [\w\-:.@%]+
+                                (
+                                    =
+                                    (?:
+                                        \\\"[^\\\"]*\\\"
+                                        |
+                                        \'[^\']*\'
+                                        |
+                                        [^\'\\\"=<>]+
+                                    )
+                                )?
+                            )
+                        )
+                    )*
+                    \s*
+                )
+            \/>
+        /x";
+
+        return preg_replace_callback($pattern, function (array $matches) {
+            $this->boundAttributes = [];
+
+            $attributes = $this->getAttributesFromAttributeString($matches['attributes']);
+
+            $component = $matches[1];
+            if (! str_contains($component, '.')) {
+                $component .= '.index';
+            }
+
+            return $this->componentString("wire::$component", $attributes)."\n@endComponentClass##END-COMPONENT-CLASS##";
+        }, $value);
+    }
+
+    /**
+     * Compile the closing tags within the given string.
+     *
+     * @return string
+     */
+    protected function compileClosingTags(string $value)
+    {
+        return preg_replace("/<\/\s*wire[\:][\w\-\:\.]*\s*>/", ' @endComponentClass##END-COMPONENT-CLASS##', $value);
+    }
+}
